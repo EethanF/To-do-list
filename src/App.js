5c6922c3538ecca4e2c5cd666c4a61a82143139b
EethanF <ethan_frediani@yahoo.co.uk>
2022-02-11 12:59:17 +0000

    y

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { useState } from 'react';
+
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [inputText, setInputText] = useState('');
+
+  const addHandler = (event) =>{
+      event.preventDefault();
+      let storedItems = [...items];
+      storedItems.push(inputText)
+      setItems(storedItems)
+  };
+  const removeHandler = (index) =>{
+      let storedItems = [...items];
+      storedItems.splice(index, 1)
+      setItems(storedItems);
+  };
+
+  return(
+      <div class = "to-do">
+          <h1>To-do list</h1>
+          <form>
+          <input type = "text" required value ={inputText} placeholder={"item"} onChange ={(event) => setInputText(event.target.value)}/>
+          <button onClick = {addHandler}>Add Item</button>
+          </form>
+          <h4>(click items to remove)</h4>
+          <p>
+          {items.map((item,index) =>{
+              return <h1 onClick={() => removeHandler(index)} key ={index}>{item}</h1>
+          })}
+          </p>
+      </div>
+  )
+};
+
 
 export default App;
